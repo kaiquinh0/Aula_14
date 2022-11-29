821a6af9400168bbb73df8653307070f7e39654e
aluno <aluno@LAB06-49>
2022-11-28 21:40:17 -0300

    Adicionar arquivos de projeto.

---
+++ Aula_14/Manipula.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_14
+{
+    public class Manipula
+    {
+        private string caminho = 
+            "C:\\Users\\Public\\Documents\\Dados.txt";
+        
+        public void Gravar(string texto)
+        {
+            StreamWriter sw = new StreamWriter(caminho);
+            sw.WriteLine(texto);
+            sw.Close();
+        } 
+        public List<Aluno> Ler()
+        {
+            var list = new List<Aluno>();
+            if (File.Exists(caminho))
+            {
+                StreamReader sr = File.OpenText(caminho);
+                string linha;
+                while((linha=sr.ReadLine()) != null)
+                {
+                    var arquivo = linha.Split(';');
+                    Aluno alguem = new Aluno(int.Parse(arquivo[0]), arquivo[1], arquivo[2], arquivo[3]);
+                    list.Add(alguem);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ Aula_14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manipula arquivo = new Manipula();
+            Aluno a = new Aluno(63222300, "Marcelo Silva", "1ºADS", "123.456.789-10");
+            arquivo.Gravar(a.RA+"; "+ a.Nome+"; "+ a.Turma+"; "+ a.Cpf);
+
+            List<Aluno> alunos = arquivo.Ler();
+            string texto = null;
+            foreach(Aluno fulano in alunos)
+            {
+                texto += "RA = " + fulano.RA + "\nNome: " + fulano.Nome + "\nTurma: "
+                    + fulano.Turma +"\nCPF: " + fulano.Cpf;
+            }
+            Console.WriteLine(texto);
+            Console.ReadKey();
+        }
+    }
+}
